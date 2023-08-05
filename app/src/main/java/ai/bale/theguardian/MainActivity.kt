b3d12cc48d77b8d3b0aece0df1271d61f761b5f0
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-08-05 18:20:48 +0330

    [Main Activity] : adding navigation and tablayout
    - implements navigation item listener
    - setup viewpage and tablayout
    
    [change]

--- app/src/main/java/ai/bale/theguardian/MainActivity.kt
+++ app/src/main/java/ai/bale/theguardian/MainActivity.kt
@@ -2,50 +2,59 @@ package ai.bale.theguardian
 
 import ai.bale.theguardian.adapter.ViewPagerAdapter
 import ai.bale.theguardian.databinding.ActivityMainBinding
-import ai.bale.theguardian.databinding.MainContentBinding
 import ai.bale.theguardian.databinding.ToolbarMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 
-private lateinit var mainBinding: ActivityMainBinding
-private lateinit var barBinding: ToolbarMainBinding
-private lateinit var contentBinding: MainContentBinding
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+    lateinit var mainBinding: ActivityMainBinding
+    lateinit var toolbarBinding: ToolbarMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //TODO
         super.onCreate(savedInstanceState)
-
+//
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        barBinding = ToolbarMainBinding.inflate(layoutInflater)
-        contentBinding = MainContentBinding.inflate(layoutInflater)
-
-        val view = mainBinding.root
-        setContentView(view)
+        toolbarBinding = ToolbarMainBinding.inflate(layoutInflater)
+//
+        setContentView(mainBinding.root)
 
 
-        val viewPager = contentBinding.viewpager
-        val tabLayout = barBinding.toolbarTabs
-
-
-        val tabsTitle = listOf<String>("Home", "World", "Science", "Sport", "Environment",
-            "Society", "Fashion", "Business", "Culture")
-
-        for(title in tabsTitle){
-            tabLayout.addTab(tabLayout.newTab().setText(title))
-        }
-
-
-
-        val toolbar = barBinding.toolbar
+        val toolbar = mainBinding.toolbar
         setSupportActionBar(toolbar)
 
+
+        //
+
+        val viewPager = mainBinding.viewpager
+        val viewAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = viewAdapter
+        viewPager.offscreenPageLimit = 2
+        //
+
+        val tabLayout = mainBinding.toolbarTabs
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Log.v("checking",tab.toString())
+                viewPager.currentItem = tab!!.position
+                tab.select()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+
+
         val drawerLayout = mainBinding.drawerLayoutMain
-        val toggleButton : ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_menu, R.string.close_menu)
+        val toggleButton = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_menu, R.string.close_menu)
         drawerLayout.addDrawerListener(toggleButton)
         toggleButton.syncState()
 
@@ -53,55 +62,56 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         navigationView.setNavigationItemSelectedListener(this)
 
 
-        val viewAdapter = ViewPagerAdapter(supportFragmentManager)
-        println("pass")
-
-
-
-
-
-        viewPager.adapter = viewAdapter
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                viewPager.currentItem = tab!!.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
-
-        })
-
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         val drawerLayout = mainBinding.drawerLayoutMain
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
-        } else{
+        } else {
             super.onBackPressed()
         }
     }
 
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val viewPager = contentBinding.viewpager
+        val viewPager = mainBinding.viewpager
         val drawerLayout = mainBinding.drawerLayoutMain
 
         when(item.itemId){
             R.id.menu_home -> {
                 viewPager.currentItem = 0
-                supportActionBar!!.title = R.string.home.toString()
             }
-
-            //TODO: .....
+            R.id.menu_world -> {
+                viewPager.currentItem = 1
+            }
+            R.id.menu_science -> {
+                viewPager.currentItem = 2
+            }
+            R.id.menu_sport -> {
+                viewPager.currentItem = 3
+            }
+            R.id.menu_environment -> {
+                viewPager.currentItem = 4
+            }
+            R.id.menu_society -> {
+                viewPager.currentItem = 5
+            }
+            R.id.menu_fashion -> {
+                viewPager.currentItem = 6
+            }
+            R.id.menu_business -> {
+                viewPager.currentItem = 7
+            }
+            R.id.menu_culture -> {
+                viewPager.currentItem = 8
+            }
         }
 
         item.isChecked = true
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
+
 }
