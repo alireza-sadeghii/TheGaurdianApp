b80e4161693305163ff38f2df9516a035b7fa6fb
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-08-05 17:52:25 +0330

    [viewPager adapter] : change getItem function implementation
    * todo : Update implementation in next edit *
    
    [change]

--- app/src/main/java/ai/bale/theguardian/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/ai/bale/theguardian/adapter/ViewPagerAdapter.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class ViewPagerAdapter(private val fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager) {
-    private val fragments = listOf<MainFragment>(MainFragment("news"),
-        MainFragment("world"), MainFragment("science"),
-        MainFragment("sport"), MainFragment("environment"),
-        MainFragment("society"), MainFragment("fashion"),
-        MainFragment("business"), MainFragment("culture"))
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
 
-    override fun getCount(): Int = fragments.size
+    override fun getCount(): Int = 9
 
-    override fun getItem(position: Int): Fragment = fragments.get(index = position)
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> MainFragment("news")
+            1 -> MainFragment("world")
+            2 -> MainFragment("science")
+            3 -> MainFragment("sport")
+            4 -> MainFragment("environment")
+            5 -> MainFragment("society")
+            6 -> MainFragment("fashion")
+            7 -> MainFragment("business")
+            8 -> MainFragment("culture")
+            else -> MainFragment("news")
+        }
+    }
+
 }
