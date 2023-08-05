46fee17db2eb378d3ace1807acc2bec549986845
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-08-05 18:17:26 +0330

    [Api Service]: change interface and retrofit creation
    - usign Gson converter for retrofit
    - change callData function that take a section name and send request
    
    [change]

--- app/src/main/java/ai/bale/theguardian/network/GuardianApiService.kt
+++ app/src/main/java/ai/bale/theguardian/network/GuardianApiService.kt
@@ -1,22 +1,17 @@
 package ai.bale.theguardian.network
 
-import ai.bale.theguardian.model.ApiResponse
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import ai.bale.theguardian.model.FetchedData
+import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 
 private const val BASE_URL = "https://content.guardianapis.com"
 
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create())
+    .addConverterFactory(GsonConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
@@ -28,13 +23,8 @@ object GuardianApi {
 }
 
 interface GuardianApiService {
-    @GET("search")
-    suspend fun callData(
-        @Query("format") format: String = "json",
-        @Query("tag") tag: String,
-        @Query("show-tags") show_tags: String = "contributor",
-        @Query("show-fields") fields: String = "starRating,headline,thumbnail",
-        @Query("show-blocks") block: String = "body",
-        @Query("api-key") key: String
-    ) : ApiResponse
+    @GET("search?format=json&show-tags=contributor&show-fields=starRating,headline,thumbnail,body&api-key=test")
+    fun callData(
+        @Query ("section") section : String
+    ): Call<FetchedData>
 }
