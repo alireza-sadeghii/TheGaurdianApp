d3a82197858ba527ae28b25bc5bc37fde660ef07
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-08-18 16:47:42 +0330

    [item viewholder]: fix issues
    - remove unnecessary fields
    - correct holding data reference
    
    [fix #11]

--- app/src/main/java/ai/bale/theguardian/adapter/ItemViewHolder.kt
+++ app/src/main/java/ai/bale/theguardian/adapter/ItemViewHolder.kt
@@ -6,6 +6,7 @@ import ai.bale.theguardian.databinding.ListItemBinding
 import ai.bale.theguardian.model.News
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -13,55 +14,27 @@ import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import org.jsoup.Jsoup
+import java.lang.Exception
 
 class ItemViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-    private val itemTitle: TextView = binding.newsTitle
-    private val itemCaption: TextView = binding.newsCaption
-    private val itemThumbnail: ImageView = binding.newsThumbnail
-    private val itemAuthor: TextView = binding.author
-    private val itemDate: TextView = binding.date
-    private val itemTag: TextView = binding.newsTag
-
-    private var new: News? = null
-
-
-    init {
-        binding.root.setOnClickListener {
-            new?.url.let { url ->
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                binding.root.context.startActivity(intent)
-            }
-        }
-    }
-
-
     fun bind(new: News?) {
         if (new != null) {
             showNew(new)
         }
     }
 
-
     private fun showNew(new: News) {
-        this.new = new
-        itemTitle.text = new.fields.title
-        itemCaption.text = Jsoup.parse(new.fields.news_body).text().take(150)
-        new.fields.image.let {
-            itemThumbnail.load(
-                new.fields.image.toUri().buildUpon().scheme("https").build()
-            )
-        }
-        itemAuthor.text = new.tags[0].getAuthor()
-        itemDate.text = new.pDate.replace("T", "\n")
-        itemTag.text = new.sectionName
-    }
-
-    companion object {
-        fun create(parent: ViewGroup): ItemViewHolder {
-            val adapterLayout = ListItemBinding
-                .inflate(LayoutInflater.from(parent.context), parent, false)
-
-            return ItemViewHolder(adapterLayout)
-        }
+        try {
+            binding.newsTitle.text = new.fields.title
+            binding.newsCaption.text = Jsoup.parse(new.fields.newsBody).text().take(150)
+            new.fields.image.let {
+                binding.newsThumbnail.load(
+                    new.fields.image.toUri().buildUpon().scheme("https").build()
+                )
+            }
+            binding.author.text = new.tags[0].getAuthor()
+            binding.date.text = new.pDate.replace("T", "\n")
+            binding.newsTag.text = new.sectionName
+        }catch (_: Exception){ }
     }
 }
