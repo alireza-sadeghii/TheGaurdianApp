add919f2805bf3acc00948141deaff06efc4c178
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-08-18 17:19:46 +0330

    [view model]: create viewmodel
    - using datarepository for making pager
    - create flow of paging data for fragment
    
    [create][fix #17]

---
+++ app/src/main/java/ai/bale/theguardian/data/NewsViewModel.kt
@@ -0,0 +1,32 @@
+package ai.bale.theguardian.data
+
+import ai.bale.theguardian.db.AppDatabase
+import ai.bale.theguardian.db.toNews
+import ai.bale.theguardian.model.News
+import ai.bale.theguardian.network.GuardianApiService
+import ai.bale.theguardian.repository.DataRepository
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class NewsViewModel(private val database: AppDatabase, private val api: GuardianApiService, private val category: String) : ViewModel() {
+    private lateinit var repository: DataRepository
+
+
+    init {
+        repository = DataRepository(apiService = api, database = database)
+    }
+
+    fun pagingFlow(): Flow<PagingData<News>> {
+        val pager = repository.providePager(category)
+        return pager
+            .flow
+            .map { data ->
+                data.map { it.toNews() }
+            }.cachedIn(viewModelScope)
+    }
+}
