133a5d1ad7b6694a77a4040a2177c2fd1a97698c
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-08-18 17:14:36 +0330

    [Tag model] : correct naming convention
    - fix first name convention
    
    [fix #15]

--- app/src/main/java/ai/bale/theguardian/model/Tag.kt
+++ app/src/main/java/ai/bale/theguardian/model/Tag.kt
@@ -3,10 +3,10 @@ package ai.bale.theguardian.model
 import com.google.gson.annotations.SerializedName
 
 data class Tag(
-    @SerializedName("firstName") val fName: String,
-    @SerializedName("lastName") val lName: String
+    @SerializedName("firstName") val firstName: String,
+    @SerializedName("lastName") val lastName: String
 ) {
     fun getAuthor(): String {
-        return "$fName $lName"
+        return "$firstName $lastName"
     }
 }
